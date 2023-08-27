92f6c2db6dc4b82453a7ca026130192c8f4cd1e3
ranchodeluxe <greg.corradini@gmail.com>
2023-08-27 09:48:55 -0700

    put up all the services

--- terraform/vars/west1-staging.tf
+++ terraform/vars/west1-staging.tf
@@ -5,4 +5,4 @@ availability_zones = ["us-west-2a", "us-west-2b"]
 dns_zone_name      = "wfs3labs.com"
 alb_protocol       = "HTTPS"
 tags               = {"project": "wfs3labs", "service": "wfs3labs"}
-dns_records        = [{ "dns_subdomain":"vector1", "ttl":300 }, { "dns_subdomain":"vector2", "ttl":300}]
+dns_records        = [{ "dns_subdomain":"vector1", "ttl":300 }, { "dns_subdomain":"vector2", "ttl":300}, { "dns_subdomain":"vector11", "ttl":300}, { "dns_subdomain":"vector12", "ttl":300}]
