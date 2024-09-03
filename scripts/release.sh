d2d574576ebdfa48be6c9e7e78e6f34e39ece341
ranchodeluxe <greg.corradini@gmail.com>
2024-09-03 09:32:30 -0700

    stuff

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,12 +4,13 @@ export RELEASE_NAME=eoapi
 export RELEASE_NS=eoapi
 export SUPPORT_RELEASE_NAME=eoapi-support
 export SUPPORT_RELEASE_NS=eoapi
+PROMETHEUS_SERVER="http://${SUPPORT_RELEASE_NAME}-prometheus-server.${SUPPORT_RELEASE_NS}.svc.cluster.local"
 
 helm upgrade --install \
   -n $SUPPORT_RELEASE_NS --create-namespace $SUPPORT_RELEASE_NAME \
   eoapi/eoapi-support --version 0.1.5 \
-  --set prometheus-adapter.prometheus.url='http://${SUPPORT_RELEASE_NAME}-prometheus-server.${SUPPORT_RELEASE_NS}.svc.cluster.local' \
-  --set grafana.datasources.datasources\\.yaml.datasources[0].url='http://${SUPPORT_RELEASE_NAME}-prometheus-server.${SUPPORT_RELEASE_NS}.svc.cluster.local' \
+  --set prometheus-adapter.prometheus.url=$PROMETHEUS_SERVER \
+  --set grafana.datasources.datasources\\.yaml.datasources[0].url=$PROMETHEUS_SERVER \
   -f /Users/ranchodeluxe/apps/eoapi-k8s/helm-chart/eoapi-support/values.yaml
 
 helm upgrade --install \
